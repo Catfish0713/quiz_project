601c9721a26d02a73f8522ba0e92119f61bfbe3f
Catfish0713 <rl60731470@gmail.com>
2024-01-16 21:57:12 -0500

    get the while loop and check_answer() attribute

--- main.py
+++ main.py
@@ -5,9 +5,16 @@ from quiz_brain import QuizBrain
 question_bank = []
 
 for i in range(len(question_data)):
-    q_text = question_data[i]["text"]
-    q_answer = question_data[i]["answer"]
+    q_text = question_data[i]["question"]
+    q_answer = question_data[i]["correct_answer"]
     question_bank.append(Question(q_text, q_answer))
 
 quiz = QuizBrain(question_bank)
 quiz.next_question()
+
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz.")
+print(f"Your final score is: {quiz.score}/{quiz.question_number}")
